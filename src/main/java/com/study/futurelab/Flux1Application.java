a2582f7a9182ad5c6c2f64e1dafa014b8ca41586
CASY82 <repaid11010@gmail.com>
2024-05-21 23:59:49 +0900

    init

---
+++ src/main/java/com/study/futurelab/Flux1Application.java
@@ -0,0 +1,13 @@
+package com.study.futurelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flux1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flux1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/futurelab/Flux1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.futurelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flux1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
